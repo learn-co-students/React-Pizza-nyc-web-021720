33eef027e5370a2b46c99bc9ab04d986e4e75a9f
brindab50 <brindab50@gmail.com>
2020-04-27 21:32:43 -0400

    Done.

--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,41 +1,70 @@
-import React from "react"
+import React from "react";
 
-const PizzaForm = () => {
-  return(
-      <div className="form-row">
-        <div className="col-5">
-            <input type="text" className="form-control" placeholder="Pizza Topping" value={
-                //Pizza Topping Should Go Here
-                null
-              }/>
+const PizzaForm = (props) => {
+  console.log(props.pizza);
+  return (
+    <div className="form-row">
+      <div className="col-5">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Pizza Topping"
+          value={props.pizza.topping}
+          onChange={(event) => {
+            props.updateTargetPizzaValue("topping", event.target.value);
+          }}
+        />
+      </div>
+      <div className="col">
+        <select
+          value={props.pizza.size}
+          className="form-control"
+          onChange={(event) => {
+            props.updateTargetPizzaValue("size", event.target.value);
+          }}
+        >
+          <option value="Small">Small</option>
+          <option value="Medium">Medium</option>
+          <option value="Large">Large</option>
+        </select>
+      </div>
+      <div className="col">
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio"
+            value="Vegetarian"
+            checked={props.pizza.vegetarian}
+            onChange={(event) => {
+              props.updateTargetPizzaValue("vegetarian", true);
+            }}
+          />
+          <label className="form-check-label">Vegetarian</label>
         </div>
-        <div className="col">
-          <select value={null} className="form-control">
-            <option value="Small">Small</option>
-            <option value="Medium">Medium</option>
-            <option value="Large">Large</option>
-          </select>
-        </div>
-        <div className="col">
-          <div className="form-check">
-            <input className="form-check-input" type="radio" value="Vegetarian" checked={null}/>
-            <label className="form-check-label">
-              Vegetarian
-            </label>
-          </div>
-          <div className="form-check">
-            <input className="form-check-input" type="radio" value="Not Vegetarian" checked={null}/>
-            <label className="form-check-label">
-              Not Vegetarian
-            </label>
-          </div>
-        </div>
-        <div className="col">
-          <button type="submit" className="btn btn-success" onClick={console.log}>Submit</button>
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio"
+            value="Not Vegetarian"
+            checked={!props.pizza.vegetarian}
+            onChange={(event) => {
+              props.updateTargetPizzaValue("vegetarian", false);
+            }}
+          />
+          <label className="form-check-label">Not Vegetarian</label>
         </div>
       </div>
+      <div className="col">
+        <button
+          type="submit"
+          className="btn btn-success"
+          onClick={props.patchPizza}
+        >
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+};
 
-  )
-}
-
-export default PizzaForm
+export default PizzaForm;
--- src/containers/PizzaList.js
+++ src/containers/PizzaList.js
@@ -1,6 +1,19 @@
-import React, { Component } from 'react';
-import Pizza from '../components/Pizza'
+import React, { Component } from "react";
+import Pizza from "../components/Pizza";
+
 class PizzaList extends Component {
+  renderPizzas = () => {
+    let pizzasToRender = this.props.pizzas;
+    return pizzasToRender.map((pizza) => {
+      return (
+        <Pizza
+          pizza={pizza}
+          key={pizza.id}
+          editPizzaForm={this.props.editPizzaForm}
+        />
+      );
+    });
+  };
 
   render() {
     return (
@@ -13,15 +26,10 @@ class PizzaList extends Component {
             <th scope="col">Edit</th>
           </tr>
         </thead>
-        <tbody>
-          {
-            //render Pizza here
-          }
-        </tbody>
+        <tbody>{this.renderPizzas()}</tbody>
       </table>
     );
   }
-
 }
 
 export default PizzaList;
